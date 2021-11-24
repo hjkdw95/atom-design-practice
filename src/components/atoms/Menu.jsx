9780d6dfced684e7deb2e96ecd420afa64ae5abc
Casey <hjkdw9506@gmail.com>
2021-11-24 14:38:13 +0900

    💄 Add : Implement Menu component

---
+++ src/components/atoms/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Menu = props => {
+  return <StyledMenu>Todo</StyledMenu>;
+};
+export default Menu;
+
+const StyledMenu = styled.a`
+  font-size: 15px;
+  line-height: 21px;
+`;
