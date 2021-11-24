b5c9bdfbac649ead22d01a4b956b97611ae918c3
Casey <hjkdw9506@gmail.com>
2021-11-24 18:04:08 +0900

    ♻️ Refactor: Refactor Input component - for more flexibility

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import theme from '../../styles/theme';
 import styled from 'styled-components';
 
-const Input = props => {
-  return <StyledInput placeholder="Click to quickly add a task"></StyledInput>;
+const Input = () => {
+  const TODO_INPUT_PLACEHOLDER = 'Click to quickly add a task';
+
+  return (
+    <InputWrapper>
+      <StyledInput placeholder={TODO_INPUT_PLACEHOLDER}></StyledInput>
+    </InputWrapper>
+  );
 };
 
 export default Input;
+
+const InputWrapper = styled.div`
+  width: 100%;
+  margin-right: 10px;
+  padding: 0 20px;
+  background-color: ${theme.white};
+  border: 1px solid;
+  border-color: ${theme.lightGrey};
+  border-radius: 20px;
+`;
 
 const StyledInput = styled.input`
   width: 100%;
