63f6873c45e9917ebb8b6e152144f902d206ce11
Casey <hjkdw9506@gmail.com>
2021-11-24 14:44:31 +0900

    💄 Add : add content title styling

--- src/components/atoms/title.jsx
+++ src/components/atoms/title.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Title = ({ content }) => {
-  return <StyledTitle>{content}</StyledTitle>;
+const Title = ({ content, className }) => {
+  return <StyledTitle className={className}>{content}</StyledTitle>;
 };
 
 export default Title;
 
 const StyledTitle = styled.h1`
   display: inline-block;
-  font-size: 20px;
-  line-height: 44px;
+  color: ${prpos => prpos.theme.black};
   font-weight: 700;
+
+  &.HeaderTitle {
+    font-size: 20px;
+    line-height: 44px;
+  }
+
+  &.contentTitle {
+    font-size: 16px;
+    line-height: 16px;
+  }
 `;
