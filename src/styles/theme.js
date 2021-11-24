47ae77f2ea884cb61bdaf6ccc7bca0f16f7ba0c4
Casey <hjkdw9506@gmail.com>
2021-11-24 13:55:25 +0900

    🎉 Begin a project

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  blue: '#0080ff',
+  black: '#030303',
+  white: '#fff',
+  lightGrey: '#d8d8d8',
+  darkGrey: '#9b9b9b',
+  shadow: 'rgba(0, 0, 0, 0.1) 0px 2px 10px 0px',
+};
+
+export default theme;
